ad9a7100dfec5aa3fa8dce4589309c99e8a4fd24
Vladimir <Vladimir@Vladimir5>
2020-03-17 19:43:28 +0200

    first commit

---
+++ ProPetsConfigServer/src/main/java/propets/ProPetsConfigServerApplication.java
@@ -0,0 +1,15 @@
+package propets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ProPetsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPetsConfigServerApplication.class, args);
+	}
+
+}
